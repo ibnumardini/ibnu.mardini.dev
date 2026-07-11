8c712ca66ac7323db9ed43862441eba863059e22
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2026-07-12 02:17:42 +0700

    feat: add sitemap integration and update robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ibnu.mardini.dev',
-	integrations: [icon()],
+	integrations: [icon(), sitemap()],
 	adapter: process.env.CLOUDFLARE ? cloudflare({ imageService: 'compile' }) : undefined,
 	vite: {
 		plugins: [tailwindcss()],
