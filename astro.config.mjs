592ac837834215899f8d91dc1d7ab2abbd97c525
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2026-07-11 14:41:26 +0700

    chore: add @astrojs/cloudflare dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ibnu.mardini.dev',
 	integrations: [icon()],
+	adapter: cloudflare({
+		imageService: 'compile',
+	}),
 	vite: {
 		plugins: [tailwindcss()],
 	},
